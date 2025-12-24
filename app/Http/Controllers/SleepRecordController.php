f43d15fbae750c53a341b5243848ccd560470838
parodez <paul.bermudez@prosperna.com>
2025-12-25 00:05:59 +0800

    Created SleepRecord model and declared relationship between User and SleepRecord

---
+++ app/Http/Controllers/SleepRecordController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SleepRecord;
+use App\Http\Requests\StoreSleepRecordRequest;
+use App\Http\Requests\UpdateSleepRecordRequest;
+
+class SleepRecordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSleepRecordRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SleepRecord $sleepRecord)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SleepRecord $sleepRecord)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSleepRecordRequest $request, SleepRecord $sleepRecord)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SleepRecord $sleepRecord)
+    {
+        //
+    }
+}
