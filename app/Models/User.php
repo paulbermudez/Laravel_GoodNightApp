56284218632ef01ed5d626d49eb606a974efe8d0
parodez <paul.bermudez@prosperna.com>
2025-12-25 23:19:09 +0800

    Added profile model with proper relationship to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\SleepRecord;
+use App\Models\Profile;
 
 class User extends Authenticatable
 {
@@ -49,5 +50,9 @@ class User extends Authenticatable
 
     public function sleepRecords() {
         return $this->hasMany(SleepRecord::class);
+    }
+
+    public function profile() {
+        return $this->hasOne(Profile::class);
     }
 }
